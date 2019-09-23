da12620ee63bb1b9d00bd0bf90b326a7678a8930
smorantg2 <51297270+smorantg2@users.noreply.github.com>
2019-09-23 22:47:06 +0200

    Add files via upload

---
+++ machine-learning-ex3/ex3/predict.m
@@ -0,0 +1,53 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+
+X = [ones(m, 1) X]; %First we need to add the column of 1's to X
+
+z2 = X * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m,1) a2];
+
+z3 = a2 * Theta2';
+
+a3 = sigmoid(z3);
+
+[pv p] = max(a3,[],2);
+
+
+%z=X*all_theta';
+%% applying sigmoid function
+%h=sigmoid(z);
+%% p1 will give highest value and p will give the index of the highest value
+%[p1 p]=max(h,[],2);
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
